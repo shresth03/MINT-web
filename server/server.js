28dfad5faca0649081bdfd8e2bf585d482313c6b
Avadhut0183 <avadhuttehare2018@gmail.com>
2026-09-22 03:56:43 +0530

    feat: prepare express api for vercel

--- server/server.js
+++ server/server.js
@@ -2,16 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { default: express } = await import("express");
-const { default: cors } = await import("cors");
-const { default: router } = await import("./routes/index.js");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api", router);
+const { default: app } = await import("./app.js");
 
 const PORT = process.env.PORT || 3000;
 
